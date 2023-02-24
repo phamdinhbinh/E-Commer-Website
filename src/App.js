1407ac83a18b52e1f340640ca5aeb8a9dcf22dfa
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-24 10:56:59 +0700

    tổ chức lại dự án

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import {BrowserRouter , Router, Route, Routes} from 'react-router-dom';
+import Layout from './Components/Layout';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='productDetail' element={<ProductDetail/>} />
+          <Route path='cart' element={<Cart/>} />
+        </Route>
+      </Routes>
+   </BrowserRouter>
+   </>
   );
 }
 
