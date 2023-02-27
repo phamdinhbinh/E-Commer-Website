94a18e6b3ac4e9e39616e0d77f94a2defbe8b5aa
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-27 14:03:22 +0700

    responsive header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,41 +1,49 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {FaUser, FaShoppingCart} from "react-icons/fa"
+import { Link } from 'react-router-dom'
+import './Style.css';
 const Header = () => {
   return (
     <>
-     <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">category</Nav.Link>
-            <Nav.Link href="#action1">category</Nav.Link>
-            <Nav.Link href="#action1">category</Nav.Link>
-            <Nav.Link href="#action1">category</Nav.Link>
-
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <nav className="navbar navbar-expand-lg bg-info bg-gradient">
+    <div className="container-fluid mx-md-2 mx-lg-5">
+    <Link className="navbar-brand  order-1 me-0 me-md-2 " href="#">Logo Brand</Link>
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse order-4 order-md-1 mt-1 mt-sm-0" id="navbarTogglerDemo02">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-1 row-cols-2  ">
+        <li className="nav-item  ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block  "  href="#">Home</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block " href="#">Link</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block" href="#">Link</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block" href="#">Link</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block" href="#">Link</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link p-1 text-dark h-100 d-inline-block" href="#">Link</Link>
+        </li>
+      </ul>
+      <form className="d-flex border border-1 rounded bg-white " role="search">
+        <input className=" form-control me-1 border border-0 " type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success m-1 border border-0 border border-start rounded-0  " type="submit">Search</button>
+      </form>
+      
+    </div>
+    <div className='order-3'>
+        <FaUser size='28px' className='mx-3' />
+        <FaShoppingCart size='28px' className='mx-1'/>
+      </div>
+  </div>
+</nav>
     </>
   )
 }
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductDetail = () => {
   return (
-    <div>ProductDetail</div>
+     <div> ProductDetail</div>
   )
 }
 
