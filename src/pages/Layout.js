fe6db6065c216dcb4c9c8f194d9622b6eade861a
dothuan2712 <dothuan2712@gmail.com>
2023-03-02 22:09:48 +0700

    revised import link in file Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom';
 import Footer from '../Components/Footer/Footer';
-import Header from '../Components/Header/Header';
+import Header from '../Components/Header/header';
 const Layout = () => {
   return (
     <>
